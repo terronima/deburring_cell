8574ad8ea499376ce022013d85efcb6ecdef2174
dhrup2781 <42072435+dhrup2781@users.noreply.github.com>
2022-11-29 12:26:47 -0500

    Add files via upload

--- BR.py
+++ BR.py
@@ -6,6 +6,10 @@ import threading
 DEBURR_L_USR_CORD_ID = 106
 BR_L_PALLET_USR_CORD = 107
 BR_R_PALLET_USR_CORD = 108
+R_1_COORD_SYS = 101
+R_2_COORD_SYS = 102
+L_1_COORD_SYS = 103
+L_2_COORD_SYS = 104
 L_F1_USR_CORD = 105
 AIRBLOW_OUTPUT = 7
 GREEN = 10
@@ -14,6 +18,7 @@ YELLOW = 12
 GREET = 0
 RIGHT_MOTOR = 13
 LEFT_MOTOR = 14
+PART_PRESENT_SENSOR = 11
 # Global variables
 Global_standby = posj(-141.6, -0.31, 142.12, 2.05, 37.75, -141.84)
 
@@ -25,6 +30,7 @@ jmove_vel = 80
 intermediate_jmove_vel = 80
 lmove_vel = 550
 convergence_vel = 100
+convergence_j_vel = 80
 convergence_acc = 200
 safe_acc = 400
 pick_acc = 300
@@ -76,7 +82,7 @@ L_F8_2 = [Global_L_F8_2_centre_j, Global_L_F8_2_centre, Global_L_F8_2_P1, Global
 R_F1 = [Global_R_F1_centre_j, Global_R_F1_centre, Global_R_F1_P1, Global_R_F1_P2, Global_R_F1_P3, Global_R_F1_P4,
         Global_R_F1_P5, Global_R_F1_P6, Global_R_F1_P7, Global_R_F1_P8, Global_R_F1_Backoff, 1]
 
-R_F2 = [Global_R_F2_centre_j, Global_R_F2_P1, Global_R_F2_P2, Global_R_F2_P3, Global_R_F2_P4, Global_R_F2_P5,
+R_F2 = [Global_R_F2_centre_j, 1, Global_R_F2_P1, Global_R_F2_P2, Global_R_F2_P3, Global_R_F2_P4, Global_R_F2_P5,
         Global_R_F2_P6, Global_R_F2_Backoff, 2]
 
 R_F2_2 = [Global_R_F2_2_centre_j, 1, Global_R_F2_2_Test, Global_R_F2_2_P1, Global_R_F2_2_P2, Global_R_F2_2_P3,
@@ -126,6 +132,7 @@ def send(msg, resp_req): #0 - send no lsten, 1 send and lsten, 2 - no send only 
             data = data.strip("z")
             #tp_log(str(data))
             return (data)
+        time.sleep(1)
 
 def send_camera_map(msg):
     data = ""
@@ -165,81 +172,132 @@ def greet():
 def pick(pos):
     # pallet_map = "100000001000000000"
     # receives string of position from camera
-    tp_log(str(pos))
+    tp_log("poaition: " +str(pos))
+    tp_log("SIDE is " + str(SIDE))
     pallet_place = int(pos)    
     side_l = None
     side_j = None
     delta_x = None
     delta_y = None
     ref_c = None
-    # assign required parameters for pick process
-    if SIDE == "L":  # if L then sets the picking position of L pallet
-        ref_c = BR_L_PALLET_USR_CORD
-        side_l = Global_pick_L_l
-        side_j = Global_pick_L_j
-        delta_x = 160 * (pallet_place // 3)
-        delta_y = 140 * (pallet_place % 3)
-    elif SIDE == "R":  # if R then sets the picking position of R pallet
-        ref_c = BR_R_PALLET_USR_CORD
-        side_l = Global_pick_R_l
-        side_j = Global_pick_R_j
-        delta_x = 160 * (pallet_place // 3)
-        delta_y = -140 * (pallet_place % 3)
-    tp_log("SIDE is " + str(SIDE))
-    tp_log("delta_x  = " + str(delta_x))
-    tp_log("delta_y   = " + str(delta_y ))
-    delta_val_above = trans(side_l, [delta_x, delta_y, 100, 0, 0, 0])
-    delta_val = trans(side_l, [delta_x, delta_y, 0, 0, 0, 0])
-    pick_pos_above = coord_transform(delta_val_above, ref_c, DR_BASE)
-    pick_pos = coord_transform(delta_val, ref_c, DR_BASE)
-    movej(side_j, vel=jmove_vel, acc=pick_acc)
-    movel(pick_pos_above, vel=lmove_vel, acc=pick_acc)
-    set_digital_output(AIRBLOW_OUTPUT, 1)
-    if get_digital_input(9) == 1:
-        movel(pick_pos, vel=convergence_vel, acc=convergence_acc)
-    else:
+
+    if (SIDE == "R" and pallet_place == 0):
+        tp_log("Executing first condition")
+        movej(Global_pick_R_0_above_j, vel=jmove_vel, acc=pick_acc)
         set_digital_output(AIRBLOW_OUTPUT, 1)
-        movel(pick_pos, vel=convergence_vel, acc=convergence_acc)
-    k_d = [500.0, 500.0, 500.0, 200.0, 200.0, 200.0]
-    task_compliance_ctrl(k_d)
-    force_desired = 10.0
-    f_d = [0.0, 0.0, -force_desired, 0.0, 0.0, 0.0]
-    f_dir = [0, 0, 1, 0, 0, 0]
-    set_desired_force(f_d, f_dir)
-    force_check = 10.0
-    while True:
-        force_condition = check_force_condition(DR_AXIS_Z, max=force_check)
-        if get_digital_input(11) == 0:
-            break
-    wait(2)
-    release_force()
-    release_compliance_ctrl()
-    set_digital_output(AIRBLOW_OUTPUT, 0)
-    if get_digital_input(10) == 1:
-        movel(pick_pos_above, vel=convergence_vel, acc=pick_acc)
-        movej(Global_BR_HOME, vel=jmove_vel, acc=pick_acc)
-    else:
+        if get_digital_input(9) == 1:
+            movej(Global_pick_R_0_j, vel=convergence_j_vel, acc=convergence_acc)
+        else:
+            set_digital_output(AIRBLOW_OUTPUT, 1)
+            movej(Global_pick_R_0_j, vel=convergence_j_vel, acc=convergence_acc)
+        wait(2)
         set_digital_output(AIRBLOW_OUTPUT, 0)
-        movel(pick_pos_above, vel=convergence_vel, acc=pick_acc)
-        movej(Global_BR_HOME, vel=jmove_vel, acc=pick_acc)
-    set_ref_coord(DR_BASE)
+        if get_digital_input(10) == 1:
+            movej(Global_pick_R_0_above_j, vel=convergence_j_vel, acc=convergence_acc)
+            movej(Global_BR_HOME, vel=jmove_vel, acc=pick_acc)
+        else:
+            set_digital_output(AIRBLOW_OUTPUT, 0)
+            wait(0.5)
+            movej(Global_pick_R_0_above_j, vel=convergence_j_vel, acc=convergence_acc)
+            movej(Global_BR_HOME, vel=jmove_vel, acc=pick_acc)
+    
+    elif (SIDE == "R" and pallet_place == 1):
+        tp_log("Executing second condition")
+        movej(Global_pick_R_1_above_j, vel=jmove_vel, acc=pick_acc)
+        set_digital_output(AIRBLOW_OUTPUT, 1)
+        if get_digital_input(9) == 1:
+            movej(Global_pick_R_1_j, vel=convergence_j_vel, acc=convergence_acc)
+        else:
+            set_digital_output(AIRBLOW_OUTPUT, 1)
+            movej(Global_pick_R_1_j, vel=convergence_j_vel, acc=convergence_acc)
+        wait(2)
+        set_digital_output(AIRBLOW_OUTPUT, 0)
+        if get_digital_input(10) == 1:
+            movej(Global_pick_R_1_above_j, vel=convergence_j_vel, acc=convergence_acc)
+            movej(Global_BR_HOME, vel=jmove_vel, acc=pick_acc)
+        else:
+            set_digital_output(AIRBLOW_OUTPUT, 0)
+            wait(0.5)
+            movej(Global_pick_R_1_above_j, vel=convergence_j_vel, acc=convergence_acc)
+            movej(Global_BR_HOME, vel=jmove_vel, acc=pick_acc)
+    
+    else:
+        # assign required parameters for pick process
+        if SIDE == "L":  # if L then sets the picking position of L pallet
+            ref_c = BR_L_PALLET_USR_CORD
+            side_l = Global_pick_L_l
+            side_j = Global_pick_L_j
+            delta_x = 160 * (pallet_place // 3)
+            delta_y = 140 * (pallet_place % 3)
+        elif SIDE == "R":  # if R then sets the picking position of R pallet
+            ref_c = BR_R_PALLET_USR_CORD
+            side_l = Global_pick_R_l
+            side_j = Global_pick_R_j
+            delta_x = 160 * (pallet_place // 3)
+            delta_y = 140 * (pallet_place % 3)
+        #tp_log("SIDE is " + str(SIDE))
+        tp_log("delta_x  = " + str(delta_x))
+        tp_log("delta_y   = " + str(delta_y ))
+        delta_val_above = trans(side_l, [delta_x, delta_y, 100, 0, 0, 0])
+        delta_val = trans(side_l, [delta_x, delta_y, 0, 0, 0, 0])
+        pick_pos_above = coord_transform(delta_val_above, ref_c, DR_BASE)
+        pick_pos = coord_transform(delta_val, ref_c, DR_BASE)
+        movej(side_j, vel=jmove_vel, acc=pick_acc)
+        movel(pick_pos_above, vel=lmove_vel, acc=pick_acc)
+        set_digital_output(AIRBLOW_OUTPUT, 1)
+        if get_digital_input(9) == 1:
+            movel(pick_pos, vel=convergence_vel, acc=convergence_acc)
+        else:
+            set_digital_output(AIRBLOW_OUTPUT, 1)
+            movel(pick_pos, vel=convergence_vel, acc=convergence_acc)
+        k_d = [500.0, 500.0, 500.0, 200.0, 200.0, 200.0]
+        task_compliance_ctrl(k_d)
+        force_desired = 10.0
+        f_d = [0.0, 0.0, -force_desired, 0.0, 0.0, 0.0]
+        f_dir = [0, 0, 1, 0, 0, 0]
+        set_desired_force(f_d, f_dir)
+        force_check = 10.0
+        while True:
+            #wait(3)
+            force_condition = check_force_condition(DR_AXIS_Z, max=force_check)
+            if get_digital_input(11) == 0:
+                break
+        wait(2)
+        release_force()
+        release_compliance_ctrl()
+        set_digital_output(AIRBLOW_OUTPUT, 0)
+        if get_digital_input(10) == 1:
+            movel(pick_pos_above, vel=convergence_vel, acc=pick_acc)
+            movej(Global_BR_HOME, vel=jmove_vel, acc=pick_acc)
+        else:
+            set_digital_output(AIRBLOW_OUTPUT, 0)
+            movel(pick_pos_above, vel=convergence_vel, acc=pick_acc)
+            movej(Global_BR_HOME, vel=jmove_vel, acc=pick_acc)
+        set_ref_coord(DR_BASE)
 
 
 # Deburr L function
 def deburr_L(*Faces, ref_c):
     global NEW_COORDINATE_SYS_FLAG
     global NEW_COORDINATE_SYS
+    new_centre_position = None
+    delta = None
+    delta_x = None
+    delta_y = None
+    delta_z = None
+    #set_digital_output(RIGHT_MOTOR, 1)
+    #set_digital_output(LEFT_MOTOR, 0)
     for m in Faces:
         Face_points = []
         L_Face = []
         L_Face = m
         L_F_j_position = L_Face[0]
         L_F_centre_position = L_Face[1]
-        movej(L_F_j_position, vel=jmove_vel, acc=safe_acc)
-        if not NEW_COORDINATE_SYS_FLAG:
-            # movej(Global_L_safe)
+        amovej(L_F_j_position, vel=jmove_vel, acc=safe_acc)
+        mwait(0)
+        if not NEW_COORDINATE_SYS_FLAG:  
+            set_ref_coord(ref_c)          
             movel(L_F_centre_position, vel=lmove_vel, acc=safe_acc)
-            set_ref_coord(L_F1_USR_CORD)
             k_d1 = [1500.0, 1500.0, 1500.0, 1000.0, 1000.0, 1000.0]
             task_compliance_ctrl(k_d1)
             force_desired = 1
@@ -247,17 +305,20 @@ def deburr_L(*Faces, ref_c):
             f_dir = [0, 0, 1, 0, 0, 0]
             set_desired_force(f_d, f_dir)
             while (1):
+                #wait(3)
                 force_condition = check_force_condition(DR_AXIS_Z, max=force_desired)
                 if force_condition == 1:
                     break
             release_compliance_ctrl()
             new_centre_position = get_current_posx()
             new_centre_position = new_centre_position[0]
-            new_centre_position = trans(new_centre_position, [0, 0,5, 0, 0, 0], L_F1_USR_CORD)
-            NEW_COORDINATE_SYS = set_user_cart_coord(new_centre_position, ref=DR_BASE)
-            NEW_COORDINATE_SYS_FLAG = 1
-            tp_log(str(NEW_COORDINATE_SYS))
-        set_ref_coord(NEW_COORDINATE_SYS)
+            tp_log("ref coord position: " + str(new_centre_position))
+            NEW_COORDINATE_SYS_FLAG = 1    
+            delta = subtract_pose(new_centre_position, L_F_centre_position)
+            tp_log("delta: " + str(delta))
+        delta_x = delta[0]
+        delta_y = delta[1]
+        delta_z = delta[2]-5
         if L_Face[len(L_Face) - 1] == 1:
             movej(Global_L_F1_Intermediate, vel=intermediate_jmove_vel, acc=intermediate_acc)
             L_Face.pop()
@@ -281,21 +342,28 @@ def deburr_L(*Faces, ref_c):
             movej(Global_L_F8_Intermediate, vel=intermediate_jmove_vel, acc=intermediate_acc)
             L_Face.pop()
         for i in range(2, len(L_Face) - 2):
-            L_Face_point = coord_transform(L_Face[i], DR_BASE, NEW_COORDINATE_SYS)
+            L_Face_point = trans(L_Face[i],[delta_x, delta_y, delta_z, 0, 0, 0], ref_c, ref_c)
             Face_points.append(L_Face_point)
-        movesx(Face_points, vel=deburr_vel, acc=deburr_acc)
-        backoff_pos = coord_transform(L_Face[len(L_Face) - 1], DR_BASE, NEW_COORDINATE_SYS)
-        movel(backoff_pos, vel=lmove_vel, acc=safe_acc, ref=NEW_COORDINATE_SYS)
+        backoff_pos = trans(L_Face[len(L_Face) - 1], [delta_x, delta_y, delta_z, 0, 0, 0], ref_c, ref_c)
+        set_ref_coord(ref_c)
+        movesx(Face_points, vel=deburr_vel, acc=deburr_acc, ref=ref_c)
+        amovel(backoff_pos, vel=lmove_vel, acc=safe_acc, ref=ref_c)
+        mwait(0)
         wait(0.5)
-        release_force
-    set_ref_coord(DR_BASE)
+        release_force  
     NEW_COORDINATE_SYS_FLAG = 0
+    set_ref_coord(DR_BASE)
 
 
 # Deburr R function
 def deburr_R(*Faces, ref_c):
     global NEW_COORDINATE_SYS_FLAG
     global NEW_COORDINATE_SYS
+    new_centre_position = None
+    delta = None
+    delta_x = None
+    delta_y = None
+    delta_z = None
     #set_digital_output(RIGHT_MOTOR, 1)
     #set_digital_output(LEFT_MOTOR, 0)
     for m in Faces:
@@ -304,11 +372,11 @@ def deburr_R(*Faces, ref_c):
         R_Face = m
         R_F_j_position = R_Face[0]
         R_F_centre_position = R_Face[1]
-        movej(R_F_j_position, vel=jmove_vel, acc=safe_acc)
-        if not NEW_COORDINATE_SYS_FLAG:
-            # movej(Global_L_safe)
+        amovej(R_F_j_position, vel=jmove_vel, acc=safe_acc)
+        mwait(0)        
+        if not NEW_COORDINATE_SYS_FLAG:  
+            set_ref_coord(ref_c)          
             movel(R_F_centre_position, vel=lmove_vel, acc=safe_acc)
-            set_ref_coord(L_F1_USR_CORD)
             k_d1 = [1500.0, 1500.0, 1500.0, 1000.0, 1000.0, 1000.0]
             task_compliance_ctrl(k_d1)
             force_desired = 1
@@ -316,17 +384,20 @@ def deburr_R(*Faces, ref_c):
             f_dir = [0, 0, 1, 0, 0, 0]
             set_desired_force(f_d, f_dir)
             while (1):
+                #wait(3)
                 force_condition = check_force_condition(DR_AXIS_Z, max=force_desired)
                 if force_condition == 1:
                     break
             release_compliance_ctrl()
             new_centre_position = get_current_posx()
             new_centre_position = new_centre_position[0]
-            new_centre_position = trans(new_centre_position, [0, 0,5, 0, 0, 0], L_F1_USR_CORD)
-            NEW_COORDINATE_SYS = set_user_cart_coord(new_centre_position, ref=DR_BASE)
-            NEW_COORDINATE_SYS_FLAG = 1
-            tp_log(str(NEW_COORDINATE_SYS))
-        set_ref_coord(NEW_COORDINATE_SYS)
+            tp_log("ref coord position: " + str(new_centre_position))
+            NEW_COORDINATE_SYS_FLAG = 1    
+            delta = subtract_pose(new_centre_position, R_F_centre_position)
+            tp_log("delta: " + str(delta))
+        delta_x = delta[0]
+        delta_y = delta[1]
+        delta_z = delta[2]-3
         if R_Face[len(R_Face) - 1] == 1:
             movej(Global_R_F1_Intermediate, vel=intermediate_jmove_vel, acc=intermediate_acc)
             R_Face.pop()
@@ -340,15 +411,19 @@ def deburr_R(*Faces, ref_c):
             movej(Global_R_F2_2_Intermediate, vel=intermediate_jmove_vel, acc=intermediate_acc)
             R_Face.pop()
         for i in range(2, len(R_Face) - 2):
-            R_Face_point = coord_transform(R_Face[i], DR_BASE, NEW_COORDINATE_SYS)
+            R_Face_point = trans(R_Face[i],[delta_x, delta_y, delta_z, 0, 0, 0], ref_c, ref_c)
             Face_points.append(R_Face_point)
-        movesx(Face_points, vel=deburr_vel, acc=deburr_acc)
-        backoff_pos = coord_transform(R_Face[len(R_Face) - 1], DR_BASE, NEW_COORDINATE_SYS)
-        movel(backoff_pos, vel=lmove_vel, acc=safe_acc, ref=NEW_COORDINATE_SYS)
+        tp_log ("Face Points:" +str(Face_points))
+        backoff_pos = trans(R_Face[len(R_Face) - 1], [delta_x, delta_y, delta_z, 0, 0, 0], ref_c, ref_c)
+        tp_log("Backoff pos: " +str(backoff_pos))
+        set_ref_coord(ref_c)
+        movesx(Face_points, vel=deburr_vel, acc=deburr_acc, ref=ref_c)
+        amovel(backoff_pos, vel=lmove_vel, acc=safe_acc, ref=ref_c)
+        mwait(0)
         wait(0.5)
-        release_force
-    set_ref_coord(DR_BASE)
+        release_force  
     NEW_COORDINATE_SYS_FLAG = 0
+    set_ref_coord(DR_BASE)
 
 
 def handover():
--- BR_main.py
+++ BR_main.py
@@ -7,8 +7,12 @@ cntr = 0
 tp_log("Now greeting...")
 if GREET == 0:
     tp_log("Greet complete!")
+    move_home(DR_HOME_TARGET_USER)
     greet()
     GREET = 1
+
+# if not (get_digital_input(PART_PRESENT_SENSOR)):
+#     movej(Global_part_remove)
 
 while True:
     tp_log("Entering big loop...")
@@ -20,33 +24,39 @@ while True:
         # request camera map
         while True:
             camera_map = send("r1,cam,r1_send_cam_data", 1)
+            time.sleep(1)
             camera_map = camera_map.strip("z")
+            #camera_map = "001000000000000000"
             if len(camera_map) == 18:
                 break
-        tp_log("Original_camera_map before" + str(camera_map))
-        
+        tp_log("Original_camera_map before" + str(camera_map))        
         tp_log("Original_camera_map " + str(camera_map))
+        
 
         # modify Right pallet data in camera map   
         pallet_map = []
         tp_log("cleared pallet map: " + str(pallet_map))
-        R_pallet_map = camera_map[0:9]
-        New_R_pallet_map = ""
-        j = 2
-        for i in range(0, int(len(R_pallet_map))):
-            New_R_pallet_map += str(R_pallet_map[j])
-            j -= 1
-            if j == -1:
-                j = 5
-            elif j == 2:
-                j = 8
-        pallet_map_str = New_R_pallet_map + camera_map[9::]
+        # R_pallet_map = camera_map[0:9]
+        # New_R_pallet_map = ""
+        # j = 2
+        # for i in range(0, int(len(R_pallet_map))):
+        #     New_R_pallet_map += str(R_pallet_map[j])
+        #     j -= 1
+        #     if j == -1:
+        #         j = 5
+        #     elif j == 2:
+        #         j = 8
+        pallet_map_str = camera_map
         for i in pallet_map_str:
             pallet_map.append(int(i))
         tp_log("updated_pallet_map " + str(pallet_map))
-        PICK_MODE = send("r1,HMI,PICK_MODE", 1)   
-        PICK_MODE = PICK_MODE.strip("z") 
-        tp_log("Pick mode:" + PICK_MODE) 
+        while True:
+            PICK_MODE = send("r1,HMI,PICK_MODE", 1)   
+            time.sleep(1)
+            PICK_MODE = PICK_MODE.strip("z")
+            tp_log("Pick mode:" + PICK_MODE) 
+            if  PICK_MODE in [ "left_only", "right_only", "intermittent", "side_by_side"]:
+                break
         while True:       
             pick_pos = None
             if PICK_MODE == "left_only":
@@ -66,12 +76,14 @@ while True:
 
             if SIDE == "L":
                 #set_digital_output(LEFT_MOTOR, 1)
-                deburr_L(L_F1, L_F2, L_F3, L_F5, L_F7, L_F8_2, ref_c = DR_BASE)
-                deburr_L(L_F6, L_F4, L_F7_2, L_F8, ref_c = DR_BASE)
+                deburr_L(L_F1, L_F2, L_F3, L_F5, L_F7, L_F8_2, ref_c = L_1_COORD_SYS)
+                deburr_L(L_F6, L_F4, L_F7_2, L_F8, ref_c = L_2_COORD_SYS)
+                #pass
             elif SIDE == "R":
                 #set_digital_output(RIGHT_MOTOR, 1)
-                deburr_R(R_F1, R_F2, R_F3, R_F5, R_F7, ref_c = DR_BASE)
-                deburr_R(R_F6, R_F4, R_F8, ref_c = DR_BASE)
+                deburr_R(R_F1, R_F2, R_F3, R_F5, R_F7, ref_c = R_1_COORD_SYS)
+                deburr_R(R_F6, R_F4, R_F8, ref_c = R_2_COORD_SYS)
+                #pass
                 
             if PICK_MODE == "left_only":
                 L_pallet_sum = 0
@@ -107,7 +119,8 @@ while True:
                     if L_pallet_sum == 0:
                         set_digital_output(LEFT_MOTOR, 0)
             handover()
-            set_digital_output(7, 1)
+            #movej(Global_handover_j, vel=jmove_vel, acc=safe_acc)
+            #set_digital_output(7, 1)
             
             if SIDE == "R":
                 cntr = pick_pos
